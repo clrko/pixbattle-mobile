6ceb62ae1b411bf1b995c75f804d32dff5c288ae
Claire Kodia <claire.kodia@gmail.com>
2021-12-14 10:20:11 +0100

    set up eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'plugin:"@react-native-community"',
+  ],
+  plugins: ['react', 'react-hooks', 'prettier'],
+};
